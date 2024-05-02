da3dbd2b177f4eca3cc094c3eee7509719fe9358
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-02 22:12:38 +0200

    Updated service status indicators and tab titles
    In the `StatusCard.razor` file, updated the service status indicator and name, and added a new service status. In the `Home.razor` file, updated the tab titles and added new areas and components. In the `Home.razor.css` file, added new styles. In the `Indicator.razor` and `Indicator.razor.cs` files, added new directives and components. In the `StatusType.cs` file, added new enumerations.

---
+++ Components/Home/Indicator.razor.cs
@@ -0,0 +1,44 @@
+﻿namespace StatusDashboard.Components.Home;
+
+using Microsoft.AspNetCore.Components;
+
+public partial class Indicator {
+    [Parameter] public StatusType Type { get; set; }
+
+    private string fillColor {
+        get {
+            const string pref = "var(--telekom-color-";
+            const string func = "functional-";
+            const string text = pref + "text-and-icon-" + func;
+
+            return this.Type switch {
+                StatusType.Maintenance => $"{text}informational)",
+                StatusType.MinorIssue => $"{pref}{func}warning-standard)",
+                StatusType.MajorIssue => $"{text}warning)",
+                StatusType.Outage => $"{text}danger)",
+                _ => $"{text}success)"
+            };
+        }
+    }
+
+    private RenderFragment icon {
+        get {
+            const string pref = "scale-icon-";
+
+            var name = this.Type switch {
+                StatusType.Maintenance => "service-maintanance",
+                StatusType.MinorIssue => "action-minus-circle",
+                StatusType.MajorIssue => "alert-warning",
+                StatusType.Outage => "action-circle-close",
+                _ => "action-success"
+            };
+
+            return x => {
+                x.OpenElement(0, $"{pref}{name}");
+                x.AddAttribute(1, "accessibility-title", Enum.GetName(this.Type));
+                x.AddAttribute(2, "fill", this.fillColor);
+                x.CloseElement();
+            };
+        }
+    }
+}
---
+++ Components/Home/StatusType.cs
@@ -0,0 +1,9 @@
+﻿namespace StatusDashboard.Components.Home;
+
+public enum StatusType {
+    Operational,
+    Maintenance,
+    MinorIssue,
+    MajorIssue,
+    Outage
+}
