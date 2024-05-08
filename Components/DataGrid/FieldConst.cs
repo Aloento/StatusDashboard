0a7812548b3cf5910b3ae6fa3c8488d124a033e7
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-08 17:01:04 +0200

    Added field constants, field options, page and component property settings
    Added `FieldTypes`, `SortBy` and `TextAlign` records defining string constants for field types, sorting methods and text alignment. In `FieldOption.cs`, a new `FieldOption` record has been added that defines the properties of the field options and specifies the JSON property names for each property. In `StatusCard.razor`, optimised the way the `Type` property of the `ServiceItem` component is used. In `Dev.razor`, added a new `Dev` page and set the properties of the `scale-data-grid` component.

---
+++ Components/DataGrid/FieldConst.cs
@@ -0,0 +1,27 @@
+﻿namespace StatusDashboard.Components.DataGrid;
+
+public record FieldTypes {
+    public string Actions => "actions";
+    public string Checkbox => "checkbox";
+    public string Email => "email";
+    public string Date => "date";
+    public string Graph => "graph";
+    public string Html => "html";
+    public string Link => "link";
+    public string Number => "number";
+    public string Select => "select";
+    public string Tags => "tags";
+    public string Telephone => "telephone";
+    public string Text => "text";
+}
+
+public record SortBy {
+    public string Number => "number";
+    public string Text => "text";
+}
+
+public record TextAlign {
+    public string Left => "left";
+    public string Right => "right";
+    public string Center => "center";
+}
---
+++ Components/DataGrid/FieldOption.cs
@@ -0,0 +1,50 @@
+﻿namespace StatusDashboard.Components.DataGrid;
+
+using System.Text.Json.Serialization;
+
+public record FieldOption {
+    /// <remarks>
+    /// <see cref="FieldTypes"/>.
+    /// </remarks>
+    [JsonPropertyName("type")]
+    public required string Type { get; set; }
+
+    [JsonPropertyName("label")]
+    public string? Label { get; set; }
+
+    [JsonPropertyName("maxWidth")]
+    public ushort? MaxWidth { get; set; }
+
+    [JsonPropertyName("minWidth")]
+    public ushort? MinWidth { get; set; }
+
+    [JsonPropertyName("mobileTitle")]
+    public bool? MobileTitle { get; set; } = false;
+
+    [JsonPropertyName("resizable")]
+    public bool? Resizable { get; set; } = true;
+
+    [JsonPropertyName("sortable")]
+    public bool? Sortable { get; set; } = false;
+
+    /// <remarks>
+    /// <see cref="DataGrid.SortBy"/>.
+    /// </remarks>
+    [JsonPropertyName("sortBy")]
+    public string? SortBy { get; set; }
+
+    /// <remarks>
+    /// <see cref="DataGrid.TextAlign"/>.
+    /// </remarks>
+    [JsonPropertyName("textAlign")]
+    public string? TextAlign { get; set; }
+
+    [JsonPropertyName("stretchWeight")]
+    public byte? StretchWeight { get; set; }
+
+    [JsonPropertyName("visible")]
+    public bool? Visible { get; set; }
+
+    [JsonPropertyName("width")]
+    public ushort? Width { get; set; }
+}
