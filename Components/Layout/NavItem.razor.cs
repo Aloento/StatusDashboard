09f250c3f4ead703d215690f9a894f9d886fee2e
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-28 15:45:39 +0200

    Updated variable names and components, added NavItem component and its backend logic
    In the `App.razor` file, we changed the variable name from `unpkg` to `cdn` and used it to link CSS styles and JavaScript modules. In the `TopNavBar.razor` file, we removed the `scale-telekom-nav-item` component and replaced it with a new `NavItem` component that accepts `href` and `Label` as parameters. Also, we have replaced the `NavLink` component with an `a` label for creating navigation links in mobile menus. In addition, we have added new `NavItem.razor` and `NavItem.razor.cs` files that define the front-end presentation and back-end logic of the `NavItem` component, respectively.

---
+++ Components/Layout/NavItem.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace StatusDashboard.Components.Layout;
+
+using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+public partial class NavItem {
+    [EditorRequired]
+    [Parameter]
+    public string? Label { get; set; }
+
+    public string? Href => (string?)this.AdditionalAttributes?.GetValueOrDefault("href");
+
+    public bool IsActive {
+        get {
+            var parent = typeof(NavLink);
+            var filedInf = parent.GetField("_isActive", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var isActive = (bool)filedInf!.GetValue(this)!;
+            return isActive;
+        }
+    }
+}
