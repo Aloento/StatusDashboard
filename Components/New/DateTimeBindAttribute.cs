fa1cc0d306f1fec80fddd6a4a52b89d844934447
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-29 19:35:43 +0200

    Updated form fields and validation, optimised datetime formatting
    In the `EventItem.razor` file, the datetime format was changed and the background colour of the indicators was modified. In the `NewForm.razor` file, a number of changes have been made, including the use of new input fields and selectors, the addition of validation messages for `model.Description` and `model.Start`, and the display of a date and time selector for `model.End` under certain conditions. In the `NewModel.cs` file, a number of changes were made to the `NewModel` class, including setting default values and adding properties. Finally, a new `DateTimeBindAttribute` class has been added to validate the date-time order.

---
+++ Components/New/DateTimeBindAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace StatusDashboard.Components.New;
+
+using System.ComponentModel.DataAnnotations;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class DateTimeBindAttribute(string targetPropertyName, bool isStart = false) : ValidationAttribute {
+    private string targetPropertyName { get; } = targetPropertyName;
+
+    private bool isStart { get; } = isStart;
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) {
+        var targetProperty = validationContext.ObjectType.GetProperty(this.targetPropertyName);
+        if (targetProperty is null)
+            return new($"Unknown Property: {this.targetPropertyName}");
+
+        var targetValue = targetProperty.GetValue(validationContext.ObjectInstance);
+        if (value == null || targetValue == null)
+            return ValidationResult.Success;
+
+        if (value is DateTime current && targetValue is DateTime target) {
+            if (this.isStart) {
+                if (current >= target)
+                    return new($"{validationContext.DisplayName} must be before {this.targetPropertyName}");
+            } else {
+                if (current <= target)
+                    return new($"{validationContext.DisplayName} must be after {this.targetPropertyName}");
+            }
+        } else
+            return new($"{this.targetPropertyName} must be a valid DateTime");
+
+        return ValidationResult.Success;
+    }
+}
--- Components/New/NewModel.cs
+++ Components/New/NewModel.cs
@@ -9,14 +9,17 @@ public class NewModel {
     public string? Title { get; set; }
 
     [Required]
-    public EventType? Type { get; set; }
+    public EventType Type { get; set; } = EventType.Maintenance;
 
     [StringLength(maximumLength: 200, MinimumLength = 10)]
     public string? Description { get; set; }
 
     public dynamic? Services { get; set; }
 
-    public DateTime? Start { get; set; }
+    [Required]
+    [DateTimeBind(nameof(End), true)]
+    public DateTime? Start { get; set; } = DateTime.Now;
 
+    [DateTimeBind(nameof(Start))]
     public DateTime? End { get; set; }
 }
