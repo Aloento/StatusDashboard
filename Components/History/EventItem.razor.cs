e66bd359f8decb6eb0b403270968b64f49a34a8f
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-11 23:36:10 +0200

    Optimized event display logic and time formatting
    Updates to `EventItem.razor`, `EventItem.razor.cs` and `History.razor` have been made to improve event item display logic and date and time formatting. In `EventItem.razor`, the `CultureInfo` class has been introduced to unify the setting of U.S. English culture information for formatting dates and times, and conditional display logic has been added to more accurately display event start times. New properties and logic have been added to `EventItem.razor.cs` to support improved display logic, and calls to the `EventItem` component in `History.razor` have been adjusted to pass information about the current and previous event. These changes improve the maintainability and readability of the code.

---
+++ Components/History/EventItem.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace StatusDashboard.Components.History;
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+
+public partial class EventItem {
+    [NotNull]
+    [Parameter]
+    [EditorRequired]
+    public Tuple<Services.Event?, Services.Event>? Item { get; set; }
+
+    private Services.Event? prev => this.Item.Item1;
+
+    private Services.Event curr => this.Item.Item2;
+
+    private bool isBegin {
+        get {
+            if (this.prev is null) 
+                return true;
+
+            return this.prev.Start.Month != this.curr.Start.Month;
+        }
+    }
+}
