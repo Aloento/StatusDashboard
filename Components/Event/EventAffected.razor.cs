7cc7c9705c50d466ef370810f7c271eed89d9c9d
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-14 16:20:46 +0200

    Optimize event impact display and resource management
    - In `EventAffected.razor`, the content of the static table has been removed in favor of a dynamically generated table displaying service names and affected regions.
    - EventAffected.razor.cs` introduces `Microsoft.EntityFrameworkCore`, adds a `list` dictionary for storing service names and zones, and implements asynchronous resource release.
    - Changes to `EventCard.razor` have been made to improve the display of the `<EventAffected>` component via the scrollbar container.
    - These changes were made to improve page interactivity and user experience, while optimizing application performance through asynchronous processing and resource management.

--- Components/Event/EventAffected.razor.cs
+++ Components/Event/EventAffected.razor.cs
@@ -2,6 +2,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 using Services;
 
 public partial class EventAffected {
@@ -12,9 +13,28 @@ public partial class EventAffected {
     [NotNull]
     private StatusContext? db { get; set; }
 
+    private Dictionary<string, HashSet<string>> list { get; } = new();
+
     public async ValueTask DisposeAsync() => await this.db.DisposeAsync();
 
     protected override async Task OnInitializedAsync() {
         this.db = await this.context.CreateDbContextAsync();
+        
+        var all = this.db.Events
+            .Where(x => x.Id == this.Id)
+            .SelectMany(x => x.RegionServices)
+            .Include(x => x.Service)
+            .Include(x => x.Region)
+            .AsAsyncEnumerable();
+
+        await foreach (var item in all) {
+            var service = item.Service.Name;
+            var region = item.Region.Name;
+
+            if (!this.list.TryGetValue(service, out var value))
+                this.list[service] = [region];
+            else
+                value.Add(region);
+        }
     }
 }
