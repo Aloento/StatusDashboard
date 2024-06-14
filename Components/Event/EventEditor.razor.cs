df5c050e307b369b55fe215e9c5311d8ad5c27ac
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-14 17:16:14 +0200

    Updates to EventEditor components and configuration
    Significant updates have been made to the `EventCard.razor` and `EventEditor.razor` files to enhance the functionality and flexibility of the `EventEditor` component. The main changes include:
    
    - The `EventEditor` component in `EventCard.razor` now supports receiving event objects as parameters.
    - `EventEditor.razor` introduces `Microsoft.EntityFrameworkCore` and `StatusDashboard.Services` references, implements the `IAsyncDisposable` interface, injects the `IDbContextFactory< StatusContext>`, and adapts the logic for displaying status options based on the event type.
    - The `EventEditor.razor.cs` backend code adds checks for parameter non-nullability and editing necessity, supports nullable incoming event objects, implements asynchronous release of database context, and creates database context and initializes form data during component initialization.
    - Updated the `StatusDashboard` configuration in `appsettings.Development.json` and `appsettings.json`, including removing the old configuration section and modifying the data source URL.
    
    These changes are intended to improve the component's data handling capabilities and the responsiveness of the user interface, while simplifying configuration management.

--- Components/Event/EventEditor.razor.cs
+++ Components/Event/EventEditor.razor.cs
@@ -1,12 +1,35 @@
 ﻿namespace StatusDashboard.Components.Event;
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
+using Services;
 
+// TODO
 public partial class EventEditor {
+    [NotNull]
+    [Parameter]
+    [EditorRequired]
+    public Event? Event { get; set; }
+
+    [NotNull]
     [SupplyParameterFromForm]
-    private EventForm form { get; set; } = new();
+    private EventForm? form { get; set; }
+
+    [NotNull]
+    private StatusContext? db { get; set; }
+
+    public async ValueTask DisposeAsync() => await this.db.DisposeAsync();
 
     private void submit() {
         Console.WriteLine(this.form.Title);
+    }
+
+    protected override async Task OnInitializedAsync() {
+        this.db = await this.context.CreateDbContextAsync();
+
+        this.form = new() {
+            Title = this.Event.Title,
+            Type = this.Event.Type
+        };
     }
 }
