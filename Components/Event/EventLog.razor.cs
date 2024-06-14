99ff9cebf94c6bba3bff7022b2471e3b5b9e3d75
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-14 16:36:12 +0200

    Optimizing the dynamic display of the Event Log component
    Important changes have been made to the `EventLog.razor` and `EventLog.razor.cs` files to enhance the dynamic display of the event log component. In `EventLog.razor`, the static display of event statuses and messages has been removed in favor of dynamically reading and displaying the status, UTC creation time, and messages of the event history from the `list` collection via a `foreach` loop. In addition, a reference to `Microsoft.EntityFrameworkCore` has been added to the `EventLog.razor.cs` file and a new `list` property has been introduced to store all history records retrieved from the database that are associated with an event ID. Retrieval of event history and sorting in descending order by creation time is achieved through Entity Framework Core's LINQ queries, which improves the flexibility and usability of the component.

--- Components/Event/EventLog.razor.cs
+++ Components/Event/EventLog.razor.cs
@@ -2,6 +2,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 using Services;
 
 public partial class EventLog {
@@ -12,9 +13,18 @@ public partial class EventLog {
     [NotNull]
     private StatusContext? db { get; set; }
 
+    [NotNull]
+    private History[]? list { get; set; }
+
     public async ValueTask DisposeAsync() => await this.db.DisposeAsync();
 
     protected override async Task OnInitializedAsync() {
         this.db = await this.context.CreateDbContextAsync();
+
+        this.list = await this.db.Events
+            .Where(x => x.Id == this.Id)
+            .SelectMany(x => x.Histories)
+            .OrderByDescending(x => x.Created)
+            .ToArrayAsync();
     }
 }
