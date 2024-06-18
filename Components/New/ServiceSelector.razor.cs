05c3bc6fa6c2eaef5d3d3e7df03354f14fdf7761
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-19 00:06:29 +0200

    Refactoring the ServiceSelector component to use the database
    Important changes have been made to the `ServiceSelector.razor` and `ServiceSelector.razor.cs` files to improve data management flexibility and application maintainability. The major changes include:
    
    - Removed code in `ServiceSelector.razor` that manually created a list of service items, an action that added 10 service items through a loop.
    - Added `@using` directive to `ServiceSelector.razor` to use `Microsoft.EntityFrameworkCore` and `StatusDashboard.Services`, and implemented `IAsyncDisposable` interface to asynchronously release the resources asynchronously.
    - Modified `ServiceSelector.razor` to inject instances of `IDbContextFactory<StatusContext>` and added code to convert `items` collections to queryable objects. Also modified the properties of the `FluentCheckbox` component to improve its accuracy and efficiency.
    - Added necessary references to `ServiceSelector.razor.cs` and defined the `ServiceSelector` partial class, including two non-null properties and the `DisposeAsync` method.
    - The `OnInitializedAsync` method has been rewritten to asynchronously initialize the `db` context and the `items` array to dynamically load the list of service items via database queries.
    
    These changes are intended to move away from static lists to dynamic database loading, while optimizing resource management and data access policies.

--- Components/New/ServiceSelector.razor.cs
+++ Components/New/ServiceSelector.razor.cs
@@ -1,6 +1,34 @@
 ﻿namespace StatusDashboard.Components.New;
 
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Services;
+
 public partial class ServiceSelector {
+    [NotNull]
+    private StatusContext? db { get; set; }
+
+    [NotNull]
+    private ServiceItem[]? items { get; set; }
+
+    public async ValueTask DisposeAsync() => await this.db.DisposeAsync();
+
+    protected override async Task OnInitializedAsync() {
+        this.db = await this.context.CreateDbContextAsync();
+
+        this.items = await this.db.Services
+            .OrderBy(x => x.Name)
+            .Select(x => new ServiceItem {
+                Id = x.Id,
+                Name = x.Name,
+                Regions = x.Regions
+                    .Select(r => new RegionItem {
+                        Id = r.Id,
+                        Name = r.Name
+                    }).ToList()
+            })
+            .ToArrayAsync();
+    }
 }
 
 public class ServiceItem {
