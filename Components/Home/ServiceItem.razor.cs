958da38c93c76f591ab4fd2df43030ba84bbcad6
Aloento <11802769+Aloento@users.noreply.github.com>
2024-06-24 15:36:02 +0200

    Optimized interaction and query logic for event indicators
    In `ServiceItem.razor`, the display logic for future events has been optimized by changing the original `div` element to an `a` tag and adding a navigation link to it, so that the user can jump directly to the corresponding event page by clicking the indicator. At the same time, for non-future events, a judgment on the event ID was added to decide whether to add a navigation link to the status indicator.
    
    In the back-end `ServiceItem.razor.cs` file, a new nullable integer attribute `id` is introduced to store the event ID, and the event query logic is updated in the `OnParametersSetAsync` method to include a new query for the event ID as well as filtering on completed, resolved and canceled status events. Additionally, the event information returned has been adjusted so that it now returns both the event ID, type, and start time.
    
    Finally, the `DisposeAsync` method has been added to asynchronously release database context resources when the component is uninstalled.

--- Components/Home/ServiceItem.razor.cs
+++ Components/Home/ServiceItem.razor.cs
@@ -19,6 +19,8 @@ public partial class ServiceItem {
 
     private bool future { get; set; }
 
+    private int? id { get; set; }
+
     public async ValueTask DisposeAsync() => await this.db.DisposeAsync();
 
     protected override async Task OnInitializedAsync() => this.db = await this.context.CreateDbContextAsync();
@@ -27,12 +29,16 @@ public partial class ServiceItem {
         var res = await this.db.RegionService
             .Where(x => x.Id == this.RegionService.Id)
             .SelectMany(x => x.Events)
-            .Select(x => new { x.Type, x.Start, x.Histories.OrderByDescending(h => h.Created).First().Status })
-            .Where(x => x.Status != EventStatus.Completed && x.Status != EventStatus.Resolved)
+            .Select(x => new { x.Id, x.Type, x.Start, x.Histories.OrderByDescending(h => h.Created).First().Status })
+            .Where(x => 
+                x.Status != EventStatus.Completed && 
+                x.Status != EventStatus.Resolved &&
+                x.Status != EventStatus.Cancelled)
             .OrderByDescending(x => x.Type)
-            .Select(x => new { x.Type , x.Start })
+            .Select(x => new { x.Id, x.Type, x.Start })
             .FirstOrDefaultAsync();
 
+        this.id = res?.Id;
         this.status = res?.Type ?? default;
         this.future = res?.Start > DateTime.UtcNow;
     }
