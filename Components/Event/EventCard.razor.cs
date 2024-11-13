068233f486d3bc0818f733cdd48b4383cdf72071
Aloento <11802769+Aloento@users.noreply.github.com>
2024-11-13 16:12:03 +0100

    优化 EventCard 组件的显示和代码结构
    在 EventCard.razor 文件中，修改了显示事件结束时间的标签内容，现在如果事件类型是 Maintenance 且事件状态不是 Completed，则在结束时间前显示 "(Plan)"。移除了 status 变量的显示，改为直接显示 theEvent.Status。
    
    在 EventCard.razor.cs 文件中，移除了 status 变量的定义和 DisposeAsync 方法。修改了 OnParametersSetAsync 方法的实现方式，改为使用表达式主体定义，并移除了对 status 变量的数据库查询。

--- Components/Event/EventCard.razor.cs
+++ Components/Event/EventCard.razor.cs
@@ -16,8 +16,6 @@ public partial class EventCard {
     [NotNull]
     private Event? theEvent { get; set; }
 
-    private EventStatus status { get; set; }
-
     public override async ValueTask DisposeAsync() {
         await base.DisposeAsync();
         this.OnSubmit.Unsubscribe(this.onSubmit);
@@ -33,16 +31,8 @@ public partial class EventCard {
         this.OnSubmit.Subscribe(this.onSubmit);
     }
 
-    protected override async Task OnParametersSetAsync() {
+    protected override async Task OnParametersSetAsync() =>
         this.theEvent = await this.db.Events
             .Where(x => x.Id == this.Id)
             .SingleAsync();
-
-        this.status = await this.db.Events
-            .Where(x => x.Id == this.Id)
-            .SelectMany(x => x.Histories)
-            .OrderByDescending(x => x.Created)
-            .Select(x => x.Status)
-            .FirstOrDefaultAsync();
-    }
 }
